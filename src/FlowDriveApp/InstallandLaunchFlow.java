b6822982d5a40f9883593a25859f2b7a0a655b01
sanjanabg <bgsanjana@gmail.com>
2017-10-17 14:49:37 +0100

    final

--- src/FlowDriveApp/InstallandLaunchFlow.java
+++ src/FlowDriveApp/InstallandLaunchFlow.java
@@ -8,8 +8,7 @@ import io.appium.java_client.android.AndroidElement;
 import org.testng.annotations.BeforeMethod;
 
 import java.io.File;
-
-
+import java.io.FileInputStream;
 import java.io.IOException;
 
 import java.net.URL;
@@ -17,7 +16,12 @@ import java.util.concurrent.TimeUnit;
 
 import org.apache.poi.EncryptedDocumentException;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
-
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -31,16 +35,27 @@ public class InstallandLaunchFlow {
 	public void beforeMethod()
 			throws InterruptedException, EncryptedDocumentException, InvalidFormatException, IOException {
 
-	
+		FileInputStream fis = new FileInputStream(
+				"C:\\Users\\gbsurao1\\eclipse-workspace\\FlowDrive\\src\\TestData\\testdata.xlsx");
+		Workbook wb = WorkbookFactory.create(fis);
+		Sheet sh = wb.getSheet("androiddetails");
+		Row row = sh.getRow(1);
+		
+		 DataFormatter formatter = new DataFormatter(); //creating formatter using the default locale
+		 Cell c1 = row.getCell(0);
+		 String devicename = formatter.formatCellValue(c1);
+		
+		 Cell c2 = row.getCell(1);
+		 String platformv = formatter.formatCellValue(c2);
 
-		File classpathRoot = new File(System.getProperty("user.dir"));
+	    File classpathRoot = new File(System.getProperty("user.dir"));
 		File appDir = new File(classpathRoot, "/Apps/FloowApp/");
 		File app = new File(appDir, "8b3d8e298a.apk");
 
 		DesiredCapabilities capabilities = new DesiredCapabilities();
 		capabilities.setCapability(CapabilityType.BROWSER_NAME, "");
-		capabilities.setCapability("deviceName", "C5302");
-		capabilities.setCapability("platformVersion", "4.3");
+		capabilities.setCapability("deviceName", devicename);
+		capabilities.setCapability("platformVersion", platformv);
 		capabilities.setCapability("platformName", "Android");
 		capabilities.setCapability("app", app.getAbsolutePath());
 
